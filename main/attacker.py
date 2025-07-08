59dd7c6b9d02e99f3e9b4de7563f7bd1ed51cfe8
woterr <shreyasv320@gmail.com>
2025-07-09 00:17:50 +0530

    nvm

--- main/attacker.py
+++
@@ -1,7 +0,0 @@
-from scapy.all import sniff, Raw
-
-def packet_callback(packet):
-    if packet.haslayer(Raw):
-        print("[INTERCEPTED] Raw Data:", packet[Raw].load)
-
-sniff(iface="enp0s3", filter="tcp port 3001 3002 3003", prn=packet_callback, store=0)
